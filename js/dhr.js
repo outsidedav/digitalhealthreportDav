470c7ab9a7193bb6363b229099f6c714d03bce71
Davon <larsonfilm@gmail.com>
2018-04-21 11:44:14 -0400

    davons js file

--- js/dhr.js
+++ js/dhr.js
@@ -1,6 +1,20 @@
 //javascript page
 
 
+//Terms of service didnt't read
+
+function Get(yourUrl){
+    var HTTpreq = new XMLHttpRequest();
+    HTTpreq.open('GET', yourUrl,false);
+    HTTpreq.send(null);
+    return HTTpreq.responseText;
+}
+
+yourUrl = 'https://tosdr.org/api/1/service/facebook.json';
+var json_obj = JSON.parse(Get(yourUrl));
+console.log(json_obj);
 
 
+//Pwned
 
+var pwnjson_obj = JSON.parse(get());
